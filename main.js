481a29ba2690695a3e7044c2e35e8331639053ee
jsimmons--dev <jsimmons@udel.edu>
2016-01-29 09:41:12 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var container,
+    renderer,
+    scene,
+    camera,
+    mesh,
+    start = Date.now(),
+    fov = 30;
+
+window.addEventListener('load', function () {
+    container = document.getElementById("container");
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(
+        fov,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000);
+    camera.position.z = 100;
+    camera.target = new THREE.Vector3(0, 0, 0);
+
+    scene.add(camera);
+
+    material = new THREE.ShaderMaterial({
+       	vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
+
+    mesh = new THREE.Mesh(
+        new THREE.IcosahedronGeometry(20, 4),
+        material
+    );
+
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    container.appendChild(renderer.domElement);
+
+    render();
+});
+
+function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
