ecdefa670b7297fa23c1021d6850c151951e42e2
jsimmons--dev <jsimmons@udel.edu>
2016-01-30 20:53:01 -0500

    updated to version 1

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+var codeSize = "15px";
+var vertexEdit = ace.edit("edit-vertex-shader");
+vertexEdit.setTheme("ace/theme/twilight");
+vertexEdit.getSession().setMode("ace/mode/glsl");
+vertexEdit.getSession().setUseWrapMode(true);
+vertexEdit.setKeyboardHandler("ace/keyboard/vim");
+vertexEdit.setValue(`
+		varying vec2 vUv;
+
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+		}
+				`,-1);
+document.querySelector('#edit-vertex-shader').style.fontSize = codeSize;
+
+var fragmentEdit = ace.edit("edit-fragment-shader");
+fragmentEdit.setTheme("ace/theme/twilight");
+fragmentEdit.getSession().setMode("ace/mode/glsl");
+fragmentEdit.getSession().setUseWrapMode(true);
+fragmentEdit.setKeyboardHandler("ace/keyboard/vim");
+
+fragmentEdit.setValue(`
+		varying vec2 vUv;
+
+		void main(){
+			gl_FragColor = vec4(vec3(vUv,0. ), 1. );
+		}
+				`,-1);
+document.querySelector('#edit-fragment-shader').style.fontSize = codeSize;
+
+
 var container,
     renderer,
     scene,
@@ -6,6 +38,9 @@ var container,
     start = Date.now(),
     fov = 30;
 
+var viewerWidth = window.innerWidth,
+	viewerHeight = window.innerHeight * .635;
+
 window.addEventListener('load', function () {
     container = document.getElementById("container");
 
@@ -13,7 +48,7 @@ window.addEventListener('load', function () {
 
     camera = new THREE.PerspectiveCamera(
         fov,
-        window.innerWidth / window.innerHeight,
+        viewerWidth / viewerHeight,
         1,
         10000);
     camera.position.z = 100;
@@ -22,8 +57,10 @@ window.addEventListener('load', function () {
     scene.add(camera);
 
     material = new THREE.ShaderMaterial({
-       	vertexShader: document.getElementById('vertexShader').textContent,
-        fragmentShader: document.getElementById('fragmentShader').textContent
+       //	vertexShader: document.getElementById('vertexShader').textContent,
+       // fragmentShader: document.getElementById('fragmentShader').textContent
+			vertexShader: vertexEdit.getValue(),
+			fragmentShader: fragmentEdit.getValue()
     });
 
     mesh = new THREE.Mesh(
@@ -34,7 +71,7 @@ window.addEventListener('load', function () {
     scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(viewerWidth, viewerHeight);
 
     container.appendChild(renderer.domElement);
 
@@ -45,3 +82,17 @@ function render() {
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
+
+function updateShaders(){
+	mesh.material = new THREE.ShaderMaterial({
+			vertexShader: vertexEdit.getValue(),
+			fragmentShader: fragmentEdit.getValue()
+	});
+}
+$(document).bind("keyup keydown", function(e){
+    if(e.ctrlKey && e.shiftKey  && e.keyCode == 53){
+		console.log('updating');
+		updateShaders();
+        return false;
+    }
+});
